24d862dcc8149a8c4e99313414eda6d725b0ba93
Anastasia Brederman <anastasyabrederman@mail.ru>
2023-12-28 05:16:00 +0300

    Добавлен файл с тестами

---
+++ src/test/java/testproject/TrialTest.java
@@ -0,0 +1,19 @@
+package testproject;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TrialTest {
+
+    @Test
+    public void testInitialState() {
+    }
+
+    @Test
+    public void testSingleAddition() {
+    }
+
+    @Test
+    public void testMultipleAdditions() {
+    }
+}
